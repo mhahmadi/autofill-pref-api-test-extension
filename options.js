be1d31ae71d596aa2433b941a700ad198b30db9b
Moe Ahmadi <mahmadi@chromium.org>
2018-08-08 13:48:22 -0400

    Adds Credit Card and Address autofill toggles
    Signed-off-by: Moe Ahmadi <mahmadi@chromium.org>

--- options.js
+++ options.js
@@ -1,11 +1,17 @@
-var updateToggle = function(status) {
-  document.getElementById('autofill-toggle').innerText = status ?
+var updateToggle = function(id, status) {
+  document.getElementById(id).innerText = status ?
     'Off' : 'On';
 }
 
 window.onload = function() {
   chrome.privacy.services.autofillEnabled.get({}, function(details) {
-    updateToggle(details.value)
+    updateToggle('autofill-toggle', details.value)
+  });
+  chrome.privacy.services.autofillCreditCardEnabled.get({}, function(details) {
+    updateToggle('cc-autofill-toggle', details.value)
+  });
+  chrome.privacy.services.autofillAddressEnabled.get({}, function(details) {
+    updateToggle('address-autofill-toggle', details.value)
   });
 };
 
@@ -20,8 +26,42 @@ document.getElementById('autofill-toggle').addEventListener('click', function() 
   });
 });
 
+document.getElementById('cc-autofill-toggle').addEventListener('click', function() {
+  chrome.privacy.services.autofillCreditCardEnabled.get({}, function(details) {
+    chrome.privacy.services.autofillCreditCardEnabled.set({
+      value: !details.value
+    }, function() {
+      if (chrome.runtime.lastError)
+        console.log(chrome.runtime.lastError);
+    });
+  });
+});
+
+document.getElementById('address-autofill-toggle').addEventListener('click', function() {
+  chrome.privacy.services.autofillAddressEnabled.get({}, function(details) {
+    chrome.privacy.services.autofillAddressEnabled.set({
+      value: !details.value
+    }, function() {
+      if (chrome.runtime.lastError)
+        console.log(chrome.runtime.lastError);
+    });
+  });
+});
+
 chrome.privacy.services.autofillEnabled.onChange.addListener(
   function(details) {
-    updateToggle(details.value);
+    updateToggle('autofill-toggle', details.value);
     console.log('Autofill is now ', details.value ? 'On' : 'Off');
+  });
+
+chrome.privacy.services.autofillCreditCardEnabled.onChange.addListener(
+  function(details) {
+    updateToggle('cc-autofill-toggle', details.value);
+    console.log('Autofill Credit Card is now ', details.value ? 'On' : 'Off');
+  });
+
+chrome.privacy.services.autofillAddressEnabled.onChange.addListener(
+  function(details) {
+    updateToggle('address-autofill-toggle', details.value);
+    console.log('Autofill Address is now ', details.value ? 'On' : 'Off');
   });
